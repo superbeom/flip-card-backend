fbd7694c35bbf0b3f3ab18b42d779fa53b7c8fab
superbeom <it.super.beom@gmail.com>
2020-12-29 16:28:29 +0900

    Delete code about plugin-transform-runtime

--- src/server.js
+++ src/server.js
@@ -1,7 +1,4 @@
 require("dotenv").config();
-require("@babel/core").transform("code", {
-  plugins: ["@babel/plugin-transform-runtime"],
-});
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
